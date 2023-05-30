d08bbd62eec92e6fab62528db30a90f59889f52a
Rafaelleal4 <123501809+Rafaelleal4@users.noreply.github.com>
2023-05-30 13:57:41 -0300

    Ajuste lista, quase finalizado.

--- assets/js/lista.js
+++ assets/js/lista.js
@@ -1,27 +1,21 @@
-var armaz = []
-
-function enviar(){
-let inp1 = document.getElementById("title").value
-let inp2 = document.getElementById("calendar").value
-let inp3 = document.getElementById("descricao").value
-let inp4 = document.getElementById("dono").value
-
-armaz.push(inp1)
-armaz.push(inp2)
-armaz.push(inp3)
-armaz.push(inp4)
 
 
-document.getElementById("titulo").innerHTML = ` ${armaz[0]}`
-document.getElementById("date").innerHTML = `Data de publicação: ${armaz[1]}`
-document.getElementById("description").innerHTML = `Descrição: ${armaz[2]}`
-document.getElementById("autor").innerHTML = `Autor: ${armaz[3]}`
-document.getElementById("remover").innerHTML = "Remover"
+function enviar() {
+    var inp1 = document.getElementById("title").value
+    var inp2 = document.getElementById("calendar").value
+    var inp3 = document.getElementById("descricao").value
+    var inp4 = document.getElementById("dono").value
+
+    if(inp1 == "" ||inp2 == "" ||inp3 == "" ||inp4 == ""){ 
+    alert("Escreva algo!!!")}
+    else{}
 
 }
-function remover(){
-document.getElementById("titulo").innerHTML = ""
-document.getElementById("date").innerHTML = ""
-document.getElementById("description").innerHTML = ""
-document.getElementById("autor").innerHTML = ""
+
+
+function remover() {
+    document.getElementById("titulo").innerHTML = ""
+    document.getElementById("date").innerHTML = ""
+    document.getElementById("description").innerHTML = ""
+    document.getElementById("autor").innerHTML = ""
 }
