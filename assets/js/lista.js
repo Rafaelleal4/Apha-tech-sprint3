675cff6e0a7c367d9f6385be6485f19e9ebde520
Rafaelleal4 <123501809+Rafaelleal4@users.noreply.github.com>
2023-05-30 17:14:37 -0300

    Ajustes lista.

--- assets/js/lista.js
+++ assets/js/lista.js
@@ -1,14 +1,27 @@
 
 
 function enviar() {
-    var inp1 = document.getElementById("title").value
-    var inp2 = document.getElementById("calendar").value
-    var inp3 = document.getElementById("descricao").value
-    var inp4 = document.getElementById("dono").value
-
+    var inp1 = document.getElementById("title").value;
+    var inp2 = document.getElementById("calendar").value;
+    var inp3 = document.getElementById("descricao").value;
+    var inp4 = document.getElementById("dono").value;
     if(inp1 == "" ||inp2 == "" ||inp3 == "" ||inp4 == ""){ 
-    alert("Escreva algo!!!")}
-    else{}
+        alert("Escreva algo!!!")
+        return;
+    }
+
+
+    var armaz = document.getElementById("armaz")
+    var list = `<div id="lists">` + `<h1 id="titu_list">Titulo: ` + inp1 +`</h1>` + `<h3>Data: ` + inp2 + `</h3>` + `<h3>Descrição: ` + inp3 + `</h3>` + `<h3>Autor: ` + inp4 + `</h3>`
+
+    armaz.innerHTML += list
+
+    document.getElementById("titulo").innerHTML = ""
+    document.getElementById("date").innerHTML = ""
+    document.getElementById("description").innerHTML = ""
+    document.getElementById("autor").innerHTML = ""
+
+    
 
 }
 
