fd2a501b97f42292233caf6a4f85599a0ff40991
Rafaelleal4 <123501809+Rafaelleal4@users.noreply.github.com>
2023-06-13 13:51:27 -0300

    correções js.

--- assets/js/lista.js
+++ assets/js/lista.js
@@ -1,23 +1,19 @@
-var divId = 0;
-var data = [];
+var divcount = 0;
 
 function enviar() {
     var title = document.getElementById('title').value;
     var date = document.getElementById('date').value;
     var datacorreta = date.split("-");
     var datainvert = datacorreta.reverse().join("/");
-    data.push(datainvert)
     var description = document.getElementById('description').value;
     var name = document.getElementById('name').value;
-    console.log(datainvert)
 
     if (title == "" || date == "" || description == "" || name == "") {
         alert("Escreva algo!!!")
         return;
     }
-
     var armaz = document.getElementById('armaz');
-    var list = '<div class="newList" id="newList2' + divId + '">' + '<h2>' + title + '</h2>' + '<p>Data: ' + datainvert + '</p>' + '<p>Descrição: ' + description + '</p>' + '<p>Autor: ' + name + '</p>' + '<button id="remover" onclick="remover(' + divId + ')">Remover</button>' + '</div>';
+    var list = '<div class="newList" id="newList2' + divcount + '">' + '<h2>' + title + '</h2>' + '<p>Data: ' + datainvert + '</p>' + '<p>Descrição: ' + description + '</p>' + '<p>Autor: ' + name + '</p>' + '<button id="remover" onclick="remover(' + divcount + ')">Remover</button>' + '</div>';
 
     armaz.innerHTML += list;
 
@@ -26,7 +22,7 @@ function enviar() {
     document.getElementById('description').value = '';
     document.getElementById('name').value = '';
 
- divId++;
+    divcount++;
 }
 
 function remover(pao) {
