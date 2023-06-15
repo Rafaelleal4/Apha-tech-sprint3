cfd03492b00ffbd4378f75f79ddaaae25865b806
Rafaelleal4 <123501809+Rafaelleal4@users.noreply.github.com>
2023-06-15 17:09:09 -0300

    correções lista again .

--- assets/js/lista.js
+++ assets/js/lista.js
@@ -7,13 +7,23 @@ function enviar() {
     var datainvert = datacorreta.reverse().join("/");
     var description = document.getElementById('description').value;
     var name = document.getElementById('name').value;
+    var dataHoje = new Date();
+    var dataFormModificada = Date(date);
+
+    if(dataHoje < dataFormModificada){
+    alert("Não pode escolher uma data passada")
+    document.getElementById('title').value = '';
+    document.getElementById('date').value = '';
+    document.getElementById('description').value = '';
+    document.getElementById('name').value = '';
+    }
 
     if (title == "" || date == "" || description == "" || name == "") {
         alert("Escreva algo!!!")
         return;
     }
     var armaz = document.getElementById('armaz');
-    var list = '<div class="newList" id="newList2' + divcount + '">' + '<h2>' + title + '</h2>' + '<p>Data: ' + datainvert + '</p>' + '<p>Descrição: ' + description + '</p>' + '<p>Autor: ' + name + '</p>' + '<button id="remover" onclick="remover(' + divcount + ')">Remover</button>' + '</div>';
+    var list = '<div class="newList" id="newList2' + divcount + '">' + '<h2>Titulo: ' + title + '</h2>' + '<hr>' + '<p>Data: ' + datainvert + '</p>' + '<hr>' + '<p>Descrição: ' + description + '</p>' + '<hr>' + '<p>Autor: ' + name + '</p>' + '<hr>' + '<button id="remover" onclick="remover(' + divcount + ')">Remover</button>' + '</div>';
 
     armaz.innerHTML += list;
 
